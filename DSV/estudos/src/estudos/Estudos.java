1d109de7dcc1a76726253084d0e0af5fb6e174cc
evertoch <evertoch@outlook.com>
2022-04-07 17:31:43 -0300

    teste

---
+++ DSV/estudos/src/estudos/Estudos.java
@@ -0,0 +1,32 @@
+package estudos;
+
+import java.util.Calendar;
+
+public class Estudos {
+
+	public static void main(String[] args) {
+			
+	Calendar calendario = Calendar.getInstance();
+	int dia = calendario.get(Calendar.MONTH);
+	
+	
+	switch(dia) {
+	case 0:{System.out.println("Janeiro");
+		break;	}
+	case 1:{System.out.println("Fevereiro");
+		break;	}
+	case 2:{System.out.println("Março");
+		break;	}
+	case 3:{System.out.println("Abril");
+		break;	}
+	default: {System.out.println("INVALIDO");}
+	
+	}
+		
+		
+	}
+
+}
+
+
+
